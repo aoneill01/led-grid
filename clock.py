cced48c887b7732b281e24796ea6da09f531bf31
Andy O'Neill <aoneill01@gmail.com>
2021-04-03 12:34:20 -0400

    Update clock

--- clock.py
+++ clock.py
@@ -5,52 +5,46 @@ import time
 import os
 
 sprites = Image.open(os.path.join(os.path.dirname(__file__), "IBM_PC_V3_8x8.png"))
-def get_sprite(row, col):
-    return sprites.crop((col * 9, row * 9, (col + 1) * 9 - 1, (row + 1) * 9 - 1))
 
-def get_num(num):
-    return get_sprite(3, num)
+background = Image.new("RGB", (52, 20))
+background.paste((0, 0, 0), [0,0,background.size[0],background.size[1]])
 
-dark = Image.new("RGB", (52, 20))
-dark.paste((0, 0, 0), [0,0,dark.size[0],dark.size[1]])
-
-light = Image.new("RGB", (52, 20))
-light.paste((25, 189, 0), [0,0,light.size[0],light.size[1]])
-draw = ImageDraw.Draw(light)
-# for row in range(0, 20, 2):
-    # draw.line([(0, row), (52, row)], fill = (21, 155, 0), width = 0)
-    # draw.line([(0, row), (52, row)], fill = (7, 47, 0), width = 0)
+foreground = Image.new("RGB", (52, 20))
+foreground.paste((25, 189, 0), [0,0,foreground.size[0],foreground.size[1]])
 
 mask = Image.new("L", (52, 20))
 
-def draw(show_cursor):
+def get_sprite(row, col):
+    return sprites.crop((col * 9, row * 9, (col + 1) * 9 - 1, (row + 1) * 9 - 1))
+
+def get_char(c):
+    order = ord(c)
+    return get_sprite(order // 16, order % 16)
+
+def get_output(phrase):
+    row1 = 2
+    row2 = 11
+    positions = [(0, row1), (8, row1), (16, row1), (0, row2), (8, row2), (16, row2), (28, row1), (36, row1), (44, row1), (28, row2), (36, row2), (44, row2)]
+    return zip(positions, map(get_char, phrase))
+
+def format_clock_string(i):
     now = datetime.now()
-    mask.paste((0), [0,0,mask.size[0],mask.size[1]])
-
-    # mask.paste(get_num(now.month // 10), (0, 3))
-    # mask.paste(get_num(now.month % 10), (8, 3))
-    # mask.paste(get_sprite(2, 15), (16, 3))
-    mask.paste(get_sprite(4, 13), (0, 3))
-    mask.paste(get_sprite(6, 1), (8, 3))
-    mask.paste(get_sprite(7, 2), (16, 3))
-    mask.paste(get_num(now.day // 10), (0, 11))
-    mask.paste(get_num(now.day % 10), (8, 11))
-
-    mask.paste(get_num(now.hour // 10), (28, 3))
-    mask.paste(get_num(now.hour % 10), (36, 3))
-    mask.paste(get_sprite(3, 10), (44, 3))
-    mask.paste(get_num(now.minute // 10), (28, 11))
-    mask.paste(get_num(now.minute % 10), (36, 11))
-    if show_cursor: mask.paste(get_sprite(1, 6), (44, 11))
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    time_separators = [" ", ":"]
+    am_pm_indicator = " "
+    hour = now.hour
+    if hour > 12:
+        am_pm_indicator = "."
+        hour -= 12
+    return f"{months[now.month - 1]}/{now.day:02d}{hour:02d}{time_separators[i % 2]}{now.minute:02d}{am_pm_indicator}"
 
 def clock(queue):
     i = 0
     while queue.empty():
-        draw(i % 2 == 0)
-        # for row in range(0, 20, 2):
-        #     if i % 2 == 0: draw.line([(0, row), (52, row)], fill = (21, 155, 0), width = 0)
-        #     else: draw.line([(0, row), (52, row)], fill = (11, 80, 0), width = 0)
-        im = Image.composite(light, dark, mask)
+        mask.paste((0), [0,0,mask.size[0],mask.size[1]])
+        for position, sprite in get_output(format_clock_string(i)):
+            mask.paste(sprite, position)
+        im = Image.composite(foreground, background, mask)
         drawFrame(im.load())
         time.sleep(1)
         i += 1
